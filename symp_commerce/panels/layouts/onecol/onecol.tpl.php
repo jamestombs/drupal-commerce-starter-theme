0d2058c28d58449ac7f21740a5648019011bb35f
James Tombs <james@symposiumdigital.co.uk>
2013-06-27 22:07:59 +0100

    Remove mojo

--- symp_commerce/panels/layouts/onecol/onecol.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @file
- * Template for a 1 column panels layout.
- */
-?>
-<?php if (!empty($css_id)) { print "<div id=\"$css_id\">"; } ?>
-  <?php print $content['first']; ?>
-<?php if (!empty($css_id)) { print "</div>"; } ?>
